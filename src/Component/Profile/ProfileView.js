a1dac92bf6f41278af76f600ab8f7f05487eab2e
Ajay Patil <ajayppatil@0312gmaill.com>
2023-05-23 12:00:17 +0530

    Sidebar and ProfileView Responsive

--- src/Component/Profile/ProfileView.js
+++ src/Component/Profile/ProfileView.js
@@ -9,13 +9,12 @@ import { AiOutlineSetting } from "react-icons/ai";
 import { GlobalContext, GlobalDispatchContext } from "../../State/Context/GlobalContext";
 
 const ProfileView = () => {
-
-  const dispatch = useContext(GlobalDispatchContext)
+  const dispatch = useContext(GlobalDispatchContext);
 
   const handleClick = () => {
     dispatch({
-      type:'SET_IS_POST_MODAL_OPEN',
-      payload:{
+      type: "SET_IS_POST_MODAL_OPEN",
+      payload: {
         isPostModalOpen: true,
       },
     });
@@ -26,54 +25,51 @@ const ProfileView = () => {
     return <div>Loading user profile...</div>;
   }
 
-
+  const smallScreen = window.innerWidth <= 600 && window.innerHeight <= 644;
 
   return (
-    <div className="w-1/5 p-4 fixed h-screen bg-gradient-to-b  from-blue-200 to-pink-200 rounded-tr-3xl rounded-tl-3xl ">
-      <div className="w-full p-4 flex flex-col items-center justify-start">
-        <div className="w-20 h-20  bg-gradient-to-r p-[5px] from-[#120fc9] to-[#ff0080] rounded-full">
-          <img
-            src={user.profileImage}
-            alt="Profile"
-            className="w-full h-full rounded-full"
-          />
-        </div>
-        <div className="mt-2  text-center">
-          <div className="text-black font-semibold text-xl">
-            {user.fullname}
+    <div className="fixed h-screen ml-5 left-0 w-1/5 md:w-1/5 bg-gradient-to-b from-blue-200 to-pink-200 rounded-tr-3xl rounded-tl-3xl">
+      <div className="h-full flex flex-col">
+        <div className="flex items-center justify-center py-8">
+          <div className="w-20 h-20 bg-gradient-to-r p-[5px] from-[#120fc9] to-[#ff0080] rounded-full">
+            <img src={user.profileImage} alt="Profile" className="w-full h-full rounded-full" />
           </div>
-          <div className="text-gray-600 italic">@{user.username}</div>
         </div>
-      </div>
-      <div className="h-[0.8px]  w-full bg-slate-400" />
-      <div className="flex flex-col py-2 space-y-5 px-6 mt-1">
-        <div className="flex items-center gap-x-6 p-1 text-black hover:text-white/100 hover:bg-black/30 transition rounded cursor-pointer ">
-          <AiOutlineHome size={25} />
-          Feed
+        <div className="text-center">
+          <div className="text-black font-semibold text-xl">{user.fullname}</div>
+          <div className="text-gray-600 italic text-sm">@{user.username}</div>
         </div>
-        <div  onClick={handleClick} className="flex items-center gap-x-6 p-1 text-black hover:text-white/100 hover:bg-black/30 transition rounded cursor-pointer">
-          <VscFileSubmodule size={25} />
-          Add Post
-        </div>
-        <div className="flex items-center gap-x-6 p-1 text-black hover:text-white/100 hover:bg-black/30 transition rounded cursor-pointer">
-          <BsBookmarks size={25} />
-          My Favorites
-        </div>
-        <div className="flex items-center gap-x-6 p-1 text-black hover:text-white/100 hover:bg-black/30 transition rounded cursor-pointer">
-          <FaRegShareSquare size={25} />
-          Direct
-        </div>
-        <div className="flex items-center gap-x-6 p-1 text-black hover:text-white/100 hover:bg-black/30 transition rounded cursor-pointer">
-          <CgScreen size={25} />
-          IG Tv
-        </div>
-        <div className="flex items-center gap-x-6 p-1 text-black hover:text-white/100 hover:bg-black/30 transition rounded cursor-pointer">
-          <ImStatsBars size={25} />
-          Stats
-        </div>
-        <div className="flex items-center gap-x-6 p-1 text-black hover:text-white/100 hover:bg-black/30 transition rounded cursor-pointer">
-          <AiOutlineSetting size={25} />
-          Setting
+        <div className="items-center flex flex-col">
+          <div className="mt-3 space-y-2 ">
+            <div className="flex items-center gap-x-6 p-1 text-black hover:text-white/100 hover:bg-black/30 transition rounded cursor-pointer">
+              <AiOutlineHome size={25} />
+              {!smallScreen && <span>Feed</span>}
+            </div>
+            <div onClick={handleClick} className="flex items-center gap-x-6 p-1 text-black hover:text-white/100 hover:bg-black/30 transition rounded cursor-pointer">
+              <VscFileSubmodule size={25} />
+              {!smallScreen && <span>Add Post</span>}
+            </div>
+            <div className="flex items-center gap-x-6 p-1 text-black hover:text-white/100 hover:bg-black/30 transition rounded cursor-pointer">
+              <BsBookmarks size={25} />
+              {!smallScreen && <span>My Favorites</span>}
+            </div>
+            <div className="flex items-center gap-x-6 p-1 text-black hover:text-white/100 hover:bg-black/30 transition rounded cursor-pointer">
+              <FaRegShareSquare size={25} />
+              {!smallScreen && <span>Direct</span>}
+            </div>
+            <div className="flex items-center gap-x-6 p-1 text-black hover:text-white/100 hover:bg-black/30 transition rounded cursor-pointer">
+              <CgScreen size={25} />
+              {!smallScreen && <span>IG Tv</span>}
+            </div>
+            <div className="flex items-center gap-x-6 p-1 text-black hover:text-white/100 hover:bg-black/30 transition rounded cursor-pointer">
+              <ImStatsBars size={25} />
+              {!smallScreen && <span>Stats</span>}
+            </div>
+            <div className="flex items-center gap-x-6 p-1 text-black hover:text-white/100 hover:bg-black/30 transition rounded cursor-pointer">
+              <AiOutlineSetting size={25} />
+              {!smallScreen && <span>Setting</span>}
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/Component/Sidebar/Sidebar.js
+++ src/Component/Sidebar/Sidebar.js
@@ -2,10 +2,8 @@ import React, { useEffect, useState } from "react";
 import { collection, onSnapshot, query } from "firebase/firestore";
 import { db } from "../../Firebase/Config";
 
-
 const Sidebar = ({ username }) => {
   const [suggestedUsers, setSuggestedUsers] = useState([]);
-  
 
   useEffect(() => {
     const usersCollection = collection(db, "users");
@@ -18,26 +16,26 @@ const Sidebar = ({ username }) => {
   }, [username]);
 
   return (
-    <div className="w-1/5 p-4 fixed h-screen bg-gradient-to-b from-blue-900 to-pink-500 rounded-tl-3xl rounded-tr-3xl ">
-      <div className="py-3 w-auto">
-        <div className="flex items-center justify-center py-2 font-semibold text-xl">
-          Suggestions for you
-        </div>
-        <div className="py-5 px-4 space-y-4">
-          {suggestedUsers.map((user) => (
-            <div
-              className="flex items-center justify-between cursor-pointer"
-              key={user.username}
-            >
-              <div className="text-white">{user.fullname}</div>
-              <button className="px-3 bg-blue-500 text-white rounded-md hover:bg-blue-600 hover:text-white transition-colors duration-300">
-                follow +
-              </button>
-            </div>
-          ))}
+      <div className="w-1/5 p-4 fixed h-screen bg-gradient-to-b from-blue-900 to-pink-500 rounded-tl-3xl rounded-tr-3xl">
+        <div className="py-3  w-auto">
+          <div className="flex items-center justify-center py-2 font-semibold text-xl">
+            Suggestions for you
+          </div>
+          <div className="py-4 space-y-3">
+            {suggestedUsers.map((user) => (
+              <div
+                className="flex items-center justify-around cursor-pointer"
+                key={user.username}
+              >
+                <div className="text-white">{user.fullname}</div>
+                <button className={'px-2 bg-blue-600 text-white rounded-md hover:text-white-500 transition-colors duration-300'}>
+                  Follow
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
